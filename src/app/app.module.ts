06a22ca8ad603c1192c8f5319538a4131369ba51
Harald Wiesinger <harald@moniholz.at>
2016-11-13 15:56:04 +0100

    creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+//Firebase
+import { AngularFireModule, AuthMethods, AuthProviders } from "angularfire2";
+//Material
 import { MaterialModule } from '@angular/material';
-
+//Components
 import { AppComponent } from './app.component';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyALvAG2XzTCBvRCpi3sSz2GUDnMlhdFz8o",
+    authDomain: "api-project-340883542890.firebaseapp.com",
+    databaseURL: "https://api-project-340883542890.firebaseio.com",
+    storageBucket: "api-project-340883542890.appspot.com",
+    messagingSenderId: "340883542890"
+  };
 
 @NgModule({
   declarations: [
@@ -15,7 +26,11 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule.forRoot()
+    MaterialModule.forRoot(),
+    AngularFireModule.initializeApp(firebaseConfig,{
+      provider: AuthProviders.Google,
+      method: AuthMethods.Popup
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
