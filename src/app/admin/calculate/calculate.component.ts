2a1692b191baca518c622032e1465862593425d8
Harald Wiesinger <harald@moniholz.at>
2017-01-10 18:32:56 +0100

    creation

--- src/app/admin/calculate/calculate.component.ts
+++ src/app/admin/calculate/calculate.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs/Rx';
 //Models
-import { MatchesModel } from '../../models/matches';
+import { MatchesModelAll } from '../../models/matches';
 import { TippsModel } from '../../models/tipps';
 import { MembersModel } from '../../models/members';
 //Services
@@ -19,7 +19,7 @@ import { MembersService } from '../../services/members.service';
 })
 export class AdminCalculateComponent {
 
-  private matchesmodelAll: MatchesModel[];
+  private matchesmodelAll: MatchesModelAll[];
   private tippsmodelAll: TippsModel[];
   private membersmodelAll: MembersModel[];
 
@@ -32,13 +32,25 @@ export class AdminCalculateComponent {
   calc(): void {
     this.matchesService.getAll().subscribe( match => {
       this.matchesmodelAll = match;
-      this.membersService.getAll().subscribe( member => {
-        this.membersmodelAll = member;
-        this.tippsService.getAllUsers(member.$key).subscribe( tipp => {
-          this.tippsmodelAll = tipp;
+      this.membersService.getAll().subscribe( members => {
+        members.forEach( member => {
+          this.tippsService.getAllUsers(member['$key']).subscribe( tipps => {
+            tipps.forEach(tipp => {
+              let points = this.calcMatch(tipp);
+            })
+          })
         })
       })
     })
+  }
+
+  calcMatch(tipp: TippsModel): number {
+    let matchpoints: number;
+    let matchmerge = this.matchesmodelAll.find(match => match['$key'] == tipp.match);
+
+    //calculate the points here
+
+    return matchpoints;
   }
 
 }
